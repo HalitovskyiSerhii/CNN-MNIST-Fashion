287b7fd7036c0829adbde63f9c308b85c454c717
Halitovskyi Serhii <halit.sergey@gmail.com>
2019-02-11 23:27:28 +0200

    Fixed.

--- cnn.py
+++ cnn.py
@@ -19,19 +19,19 @@ fashion_mnist = keras.datasets.fashion_mnist
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-
 train_images = train_images / 255.0
 
-train_images = train_images.reshape((train_images.shape[0],28,28,1))
+train_images = train_images.reshape((train_images.shape[0], 28, 28, 1))
 
 test_images = test_images / 255.0
 
-test_images = test_images.reshape((test_images.shape[0],28,28,1))
+test_images = test_images.reshape((test_images.shape[0], 28, 28, 1))
 
 print(train_images.shape)
 
 model = keras.Sequential([
 
+    keras.layers.InputLayer(input_shape=(28, 28, 1)),
     keras.layers.BatchNormalization(),
 
     keras.layers.Conv2D(input_shape=(28, 28, 1),
@@ -39,7 +39,6 @@ model = keras.Sequential([
                         kernel_size=4,
                         padding="same",
                         activation=tf.nn.relu),
-
 
     keras.layers.MaxPooling2D(pool_size=[2, 2], strides=2),
 
@@ -92,12 +91,12 @@ partial_train_images = train_images[15000:]
 partical_train_labels = train_labels[15000:]
 
 file_name = str(__file__)[:-3]
-weights_file = os.path.join( 'data', 'weights', file_name)
+weights_file = os.path.join('data', 'weights', file_name)
 print(weights_file)
 
 if not os.path.exists(weights_file):
 
-    open(os.path.abspath(weights_file),'w').close()
+    open(os.path.abspath(weights_file), 'w').close()
 
     history = model.fit(partial_train_images, partical_train_labels, epochs=20,
                         batch_size=64, validation_data=(val_images, val_labels), verbose=1,
